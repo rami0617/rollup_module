49b96885398b2ec99e46b781c32e505da89e6df5
최아람 <aarmakrr@hanmail.net>
2024-03-11 23:31:51 +0900

    minifi 라이브러리 적용

--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,13 @@
 import typescript from "@rollup/plugin-typescript";
+import terser from "@rollup/plugin-terser";
 
 export default {
   input: "src/main.ts",
   output: {
     file: "dist/bundle.js",
     format: "es",
-    sourcemap: true,
+    name: "myBundle",
+    exports: "auto",
   },
-  plugins: [typescript()],
+  plugins: [typescript(), terser()],
 };
